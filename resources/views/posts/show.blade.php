4c0cbe070b59002047200981529a172f32fcbdaa
Rajeev Jha <torajeev86@gmail.com>
2025-01-08 00:17:30 +0530

    to test feature branch

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <h1>{{ $post->title }}</h1>
+        <h2>{{ $post->title }}</h2>
         <p>{{ $post->content }}</p>
         <a href="{{ route('posts.index') }}" class="btn btn-primary">Back to Posts</a>
     </div>
