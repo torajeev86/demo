2abb9a57c93eeed837e2d0baf4a8e168af4ed04a
Rajeev Jha <torajeev86@gmail.com>
2025-02-07 16:06:22 +0530

    design updated

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,19 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Create Post</h1>
-        <form action="{{ route('posts.store') }}" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="title">Title</label>
-                <input type="text" class="form-control" id="title" name="title" required>
+<div class="container">
+    <h1>Create Post</h1>
+    <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
-            <div class="form-group">
-                <label for="content">Content</label>
-                <textarea class="form-control" id="content" name="content" rows="4" required></textarea>
-            </div>
-            <button type="submit" class="btn btn-success mt-3">Create Post</button>
-        </form>
-    </div>
+        @endif
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" id="title" name="title" required>
+        </div>
+        <div class="form-group">
+            <label for="content">Content</label>
+            <textarea class="form-control" id="content" name="content" rows="4" required></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="formFile" class="form-label">Image Upload</label>
+            <input class="form-control" type="file" name="image" id="image">
+        </div>
+        <button type="submit" class="btn btn-success mt-3">Create Post</button>
+    </form>
+</div>
 @endsection
